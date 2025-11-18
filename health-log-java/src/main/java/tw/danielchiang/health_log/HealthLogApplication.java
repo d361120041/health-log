26f6c61ff16dce760df0b4ba17c4795a4cf42151
daniel <d361120041@yahoo.com.tw>
2025-11-18 23:41:46 +0800

    first commit

---
+++ health-log-java/src/main/java/tw/danielchiang/health_log/HealthLogApplication.java
@@ -0,0 +1,13 @@
+package tw.danielchiang.health_log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthLogApplication.class, args);
+	}
+
+}
---
+++ health-log-java/src/test/java/tw/danielchiang/health_log/HealthLogApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.danielchiang.health_log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
