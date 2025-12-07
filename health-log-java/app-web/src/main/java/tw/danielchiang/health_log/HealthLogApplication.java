11893fe58b12cee4036811875a1b97abe48007a9
daniel <d361120041@yahoo.com.tw>
2025-12-07 21:52:27 +0800

    Add Spring Boot Actuator dependency, update JPA repository and entity scanning configurations, and modify PostgreSQL datasource URL in application properties.

--- health-log-java/app-web/src/main/java/tw/danielchiang/health_log/HealthLogApplication.java
+++ health-log-java/app-web/src/main/java/tw/danielchiang/health_log/HealthLogApplication.java
@@ -2,10 +2,12 @@ package tw.danielchiang.health_log;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "tw.danielchiang.health_log")
-@EnableJpaRepositories(basePackages = "tw.danielchiang.health_log")
+@EnableJpaRepositories(basePackages = "tw.danielchiang.health_log.data.repository")
+@EntityScan(basePackages = "tw.danielchiang.health_log.model.entity")
 public class HealthLogApplication {
 
 	public static void main(String[] args) {
